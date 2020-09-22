6aee5d92b9fb55f24b56fcd0be51d46c154026f4
izhanh <izhan2@msn.com>
2020-09-22 15:57:50 +0200

    changes

--- integration-test-example/test.sh
+++ integration-test-example/test.sh
@@ -3,7 +3,13 @@
 sleep 3
 if curl web:5000 | grep -q 'Hola'; then
   echo "Tests passed!"
-  exit 0
+else
+  echo "Tests failed!"
+  exit 1
+fi
+
+if curl web:5000 | grep -q 'Pinguino'; then
+  echo "Tests passed!"
 else
   echo "Tests failed!"
   exit 1
