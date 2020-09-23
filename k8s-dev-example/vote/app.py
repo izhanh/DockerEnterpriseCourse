b7b77f692ec048245e589695c1e75853f51815cb
izhanh <izhan2@msn.com>
2020-09-23 16:46:35 +0200

    okteto example

--- k8s-dev-example/vote/app.py
+++ k8s-dev-example/vote/app.py
@@ -5,18 +5,20 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Caballos")
+option_b = os.getenv('OPTION_B', "Perros")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
+
 
 def get_redis():
     if not hasattr(g, 'redis'):
         g.redis = Redis(host="redis", db=0, socket_timeout=5)
     return g.redis
 
-@app.route("/", methods=['POST','GET'])
+
+@app.route("/", methods=['POST', 'GET'])
 def hello():
     voter_id = request.cookies.get('voter_id')
     if not voter_id:
